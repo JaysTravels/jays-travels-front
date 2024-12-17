4363c2633559169ba9d266a401d3db6c13116366
Amjad Ismail <amjad.rahimo@live.com>
2024-12-17 17:53:32 +0500

    Added changes in Availability slice

--- store/AvailabilitySlice.js
+++ store/AvailabilitySlice.js
@@ -9,6 +9,7 @@ export const submitFlightData = createAsyncThunk(
     'flights/submitFlightData',
     async (flightData, { rejectWithValue }) => {
       try { 
+        debugger;
         console.log(flightData)
         const response = await axiosInstance.post('availability', flightData);
         console.log(response.data)      
