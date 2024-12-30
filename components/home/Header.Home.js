ae869b3c5c5e02aa5b85ef4276a12c8e76bd4080
Amjad Ismail <amjad.rahimo@live.com>
2024-12-30 08:27:48 +0500

    Images lazy loading

--- components/home/Header.Home.js
+++ components/home/Header.Home.js
@@ -34,6 +34,7 @@ const HeaderHome = () => {
                   height="50"
                   alt=""
                   className="bg-img d-none"
+                  loading="lazy"
                 />
               </div>
             </div>
@@ -50,6 +51,7 @@ const HeaderHome = () => {
                   height="50"
                   alt=""
                   className="bg-img d-none"
+                  loading="lazy"
                 />
               </div>
             </div>
@@ -66,6 +68,7 @@ const HeaderHome = () => {
                   height="50"
                   alt=""
                   className="bg-img d-none"
+                  loading="lazy"
                 />
               </div>
             </div>
