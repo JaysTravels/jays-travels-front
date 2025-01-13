ea64a53b53799304ed4e47b0b06ae49217af0108
JaysTravels <zain@jaystravels.co.uk>
2025-01-13 14:27:38 +0000

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
       {
         name: "jays-travels-front",
-        script: "./node_modules/next/dist/bin/next",
+        script: "/home/site/wwwroot/node_modules/next/dist/bin/next",
         args: "start -p " + (process.env.PORT || 3000),
         watch: false,
         autorestart: true,
