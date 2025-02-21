21548d9bf317ad651a88d243e91bdfbbbf504d05
amjadjays <amjad@jaystravels.co.uk>
2025-02-21 10:00:43 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,1 @@
-module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/:path*',
-          has: [
-            {
-              type: 'host',
-              value: 'www.jaystravels.co.uk',
-            }, 
-          ],
-          destination: 'https://jaystravels.co.uk/:path*',
-          permanent: true,
-        },
-      ];
-    },
-  };
+
