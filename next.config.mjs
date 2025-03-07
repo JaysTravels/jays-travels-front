d8f0ff0a048745212090d6571e0577edc940acb7
JaysTravels <zain@jaystravels.co.uk>
2025-03-07 23:48:35 +0000

    Non WWW Redirect Soluton

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: 'build',
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "http://www.jaystravel.co.uk/" }],
+        destination: "https://jaystravels.co.uk/:path*",
+        permanent: true,
+      },
+      // If you need more redirects, they can be added JS objects here.
+    ];
+  },
 };
 export default nextConfig;
