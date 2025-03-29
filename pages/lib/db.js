9acc28423112bbdc6f6bb56c44e9d6867b1f71f7
amjadjays <amjad@jaystravels.co.uk>
2025-03-29 16:36:45 +0500

    sort db connection string issue

--- pages/lib/db.js
+++ pages/lib/db.js
@@ -3,9 +3,9 @@ import { Pool } from "pg";
 // Create a connection pool (reuse instead of reconnecting for every request)
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL, // Use environment variable for security
-    ssl: {
-        rejectUnauthorized: false, // Required if using PostgreSQL on Azure or Heroku
-    },
+    ssl: process.env.PGSSLMODE
+    ? { rejectUnauthorized: process.env.PGSSLMODE !== 'disable' }
+    : false,
 });
 
 export default pool;
