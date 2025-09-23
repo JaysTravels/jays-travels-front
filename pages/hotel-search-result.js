51d97aed4140c43926e96beb2003c841d4b3534a
amjadjays <amjad@jaystravels.co.uk>
2025-09-23 21:06:06 +0500

    Hotel Search control

--- pages/hotel-search-result.js
+++ pages/hotel-search-result.js
@@ -2,7 +2,7 @@ import Meta from "@/components/common/Meta";
 import FrontLayout from "@/components/layouts/Front.Layout";
 import HotelBreadcrumbSr from "@/components/hotels/HotelBreadcrumb.Sr";
 import HotelContentSr from "@/components/hotels/HotelContent.Sr";
-import SearchBarSr from "@/components/SearchResult/SearchBar.Sr";
+import SearchBarSr from "@/components/SearchResult/HotelSearchBar.Sr";
 import { Breadcrumb } from "reactstrap";
 
 const HotelSearchResult = () => {
